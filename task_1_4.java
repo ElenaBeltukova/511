20a0d287b6519410a27a0be78a2e21f67a30bcae
ElenaBeltukova <82584758+ElenaBeltukova@users.noreply.github.com>
2021-04-15 15:01:11 +0300

    Add files via upload

---
+++ task_1_4.java
@@ -0,0 +1,9 @@
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
+
+public class task_1_4 {
+    public static void main(String[] args) {
+        int number = 25;
+        System.out.println(number * number);
+    }
+}
