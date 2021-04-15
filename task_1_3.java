5e6692236e57c2c62e99fb17cef826fea38b9419
ElenaBeltukova <82584758+ElenaBeltukova@users.noreply.github.com>
2021-04-15 14:57:17 +0300

    Add files via upload

---
+++ task_1_3.java
@@ -0,0 +1,14 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class task_1_3 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        String s = "abs";
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(s);
+    }
+}
