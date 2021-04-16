51431aece3a0f21b8300f0d7f1a806d27dc65049
ElenaBeltukova <82584758+ElenaBeltukova@users.noreply.github.com>
2021-04-16 10:24:13 +0300

    Add files via upload

---
+++ task_1_7.java
@@ -0,0 +1,24 @@
+/* Задача наполнить корабельный бассейн. Нужно посчитать, сколько литров воды нужно для заполнения бассейна до бортов.
+Известно, что бассейн имеет линейные размеры a × b × c, заданные в метрах.
+Эти размеры передаются запрашиваются у пользователя. Прграмма должена вывести на экран количество литров воды,
+которое нужно для наполнения бассейна.
+Пример:
+Пользователь даёт числа 25, 5, 2.
+Пример вывода:
+250000 */
+
+
+import java.util.Scanner;
+
+public class task_1_7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("a = ");
+        int a = scan.nextInt();
+        System.out.print("b = ");
+        int b = scan.nextInt();
+        System.out.print("c = ");
+        int c = scan.nextInt();
+        System.out.println("L = " + a * b * c * 1000);
+    }
+}
