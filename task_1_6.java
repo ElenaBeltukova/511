609227f96741541af537569a6756817230027857
ElenaBeltukova <82584758+ElenaBeltukova@users.noreply.github.com>
2021-04-16 10:18:26 +0300

    Add files via upload

---
+++ task_1_6.java
@@ -0,0 +1,13 @@
+/*
+Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
+*/
+
+import java.util.Scanner;
+
+public class task_1_6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(n*1.15);
+    }
+}
