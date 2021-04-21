b0dee8e7b4f9b9f5314611146ccc8ea3a05b7e8e
ElenaBeltukova <82584758+ElenaBeltukova@users.noreply.github.com>
2021-04-21 17:09:09 +0300

    Update and rename task_1_4.java to Task_1_4/Task_1_4.java

--- Task_1_4/Task_1_4.java
+++ Task_1_4/Task_1_4.java
@@ -1,7 +1,7 @@
 /* Дана переменная number.
 Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
 
-public class task_1_4 {
+public class Task_1_4 {
     public static void main(String[] args) {
         int number = 25;
         System.out.println(number * number);
