4244b78c4fa7571da1cb322b24e23de592ca9313
ElenaBeltukova <82584758+ElenaBeltukova@users.noreply.github.com>
2021-04-21 19:08:28 +0300

    Update and rename task_1_6.java to Task_1_6/Task_1_6.java

--- Task_1_6/Task_1_6.java
+++ Task_1_6/Task_1_6.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class task_1_6 {
+public class Task_1_6 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
