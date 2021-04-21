3f81682b3e4358debaa7b0379381d0f313c55a9f
ElenaBeltukova <82584758+ElenaBeltukova@users.noreply.github.com>
2021-04-21 14:58:02 +0300

    Create Main.java

---
+++ Task_4_6/Main.java
@@ -0,0 +1,23 @@
+/*
+Задача вывести на экран такое сообщение (для объекта gosha):
+Меня зовут "имя объекта"
+Имя моей мамы: "имя мамы"
+Имя моего папы: "имя папы"
+У меня две бабушки, бабушка "имя бабушки" и "имя бабушки"
+У меня два деда, деда "имя деда" и "имя деда"
+*/
+import Person.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person valera=new Person("Валера","Иванов",69,null,null);
+        Person olga = new Person("Ольга","Иванова",67,null,null);
+        Person oleg = new Person("Олег","Петров",65,null,null);
+        Person jula = new Person("Юля","Петрова",67,null,null);
+        Person alex = new Person("Алексей","Иванов",37,olga,valera);
+        Person eva  = new Person("Ева","Иванова",37,jula,oleg);
+        Person gosha= new Person("Гоша","Иванов",10,eva,alex);
+
+        System.out.println(gosha.getInfo());
+    }
+}
