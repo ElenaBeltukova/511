9004ea716fa248c930767548c4010fc90b2ee3b7
ElenaBeltukova <82584758+ElenaBeltukova@users.noreply.github.com>
2021-04-27 15:15:43 +0300

    Update Main.java

--- Task_5_1/Main.java
+++ Task_5_1/Main.java
@@ -4,19 +4,24 @@
  * 2) вывести коллекцию на экран
  * */
 
+import java.util.Set;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList();
-        names.add("Анатолий");
-        names.add("Георгий");
-        names.add("Руслан");
-        names.add("Георгий");
-        names.add("Павел");
-        names.add("Руслан");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String family = scanner.nextLine();
+            if (family.isEmpty()) {
+                break;
+            }
+
+            names.add(family);
+        }
 
         System.out.println(names);
 
